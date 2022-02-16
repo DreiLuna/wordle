a0c5026314c1bf8d1afc1c00628fe0547d372494
DreiLuna <54549805+DreiLuna@users.noreply.github.com>
2022-02-16 10:54:30 -0600

    buttons and defs for keyboard

--- digitalkeyboard.py
+++ digitalkeyboard.py
@@ -1,1 +1,90 @@
-print("hellow world")
+from tkinter import *
+root = Tk()
+
+root.geometry("1000x100")
+
+heightl = 5
+widthl = 5
+
+#!def for said buttons
+def exa():
+def exb():
+def exc():
+def exd():
+def exe():
+def exf():
+def exg():
+def exh():
+def exi():
+def exj():
+def exk():
+def exl():
+def exm():
+def exn():
+def exo():
+def exp():
+def exq():
+def exr():
+def exs():
+def ext():
+def exu():
+def exv():
+def exw():
+def exx():
+def exy():
+def exz():
+
+#!buttons
+leta = Button(root, text="a", height=heightl, width=widthl, command = exa)
+letb = Button(root, text="a", height=heightl, width=widthl, command = exb)
+letc = Button(root, text="a", height=heightl, width=widthl, command = exc)
+letd = Button(root, text="a", height=heightl, width=widthl, command = exd)
+lete = Button(root, text="a", height=heightl, width=widthl, command = exe)
+letf = Button(root, text="a", height=heightl, width=widthl, command = exf)
+letg = Button(root, text="a", height=heightl, width=widthl, command = exg)
+leth = Button(root, text="a", height=heightl, width=widthl, command = exh)
+leti = Button(root, text="a", height=heightl, width=widthl, command = exi)
+letj = Button(root, text="a", height=heightl, width=widthl, command = exj)
+letk = Button(root, text="a", height=heightl, width=widthl, command = exk)
+letl = Button(root, text="a", height=heightl, width=widthl, command = exl)
+letm = Button(root, text="a", height=heightl, width=widthl, command = exm)
+letn = Button(root, text="a", height=heightl, width=widthl, command = exn)
+leto = Button(root, text="a", height=heightl, width=widthl, command = exo)
+letp = Button(root, text="a", height=heightl, width=widthl, command = exp)
+letq = Button(root, text="a", height=heightl, width=widthl, command = exq)
+letr = Button(root, text="a", height=heightl, width=widthl, command = exr)
+lets = Button(root, text="a", height=heightl, width=widthl, command = exs)
+lett = Button(root, text="a", height=heightl, width=widthl, command = ext)
+letu = Button(root, text="a", height=heightl, width=widthl, command = exu)
+letv = Button(root, text="a", height=heightl, width=widthl, command = exv)
+letw = Button(root, text="a", height=heightl, width=widthl, command = exw)
+letx = Button(root, text="a", height=heightl, width=widthl, command = exx)
+lety = Button(root, text="a", height=heightl, width=widthl, command = exy)
+letz = Button(root, text="a", height=heightl, width=widthl, command = exz)
+
+leta.place(x=xcol1, y=yrow1)
+letb.place(x=xcol1, y=yrow1)
+letc.place(x=xcol1, y=yrow1)
+letd.place(x=xcol1, y=yrow1)
+lete.place(x=xcol1, y=yrow1)
+letf.place(x=xcol1, y=yrow1)
+letg.place(x=xcol1, y=yrow1)
+leth.place(x=xcol1, y=yrow1)
+leti.place(x=xcol1, y=yrow1)
+letj.place(x=xcol1, y=yrow1)
+letk.place(x=xcol1, y=yrow1)
+letl.place(x=xcol1, y=yrow1)
+letm.place(x=xcol1, y=yrow1)
+letn.place(x=xcol1, y=yrow1)
+leto.place(x=xcol1, y=yrow1)
+letp.place(x=xcol1, y=yrow1)
+letq.place(x=xcol1, y=yrow1)
+letr.place(x=xcol1, y=yrow1)
+leta.place(x=xcol1, y=yrow1)
+lett.place(x=xcol1, y=yrow1)
+letu.place(x=xcol1, y=yrow1)
+letv.place(x=xcol1, y=yrow1)
+letw.place(x=xcol1, y=yrow1)
+letx.place(x=xcol1, y=yrow1)
+lety.place(x=xcol1, y=yrow1)
+letz.place(x=xcol1, y=yrow1)
